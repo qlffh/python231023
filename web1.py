8a839373c13d6309efa79d0d8834d26219838caf
qlffh <kkkjy0628@ewhain.net>
2023-10-26 11:01:23 +0900

    ..

--- web1.py
+++ web1.py
@@ -3,8 +3,19 @@
 from bs4 import BeautifulSoup
 
 # 페이지 로딩
-page = open("c:\\work\\test01.html","rt", encoding = "utf-8").read()
+page = open("c:\\work\\test01.html","rt", encoding = "utf-8").read() 
+
 # 검색이 용이한 객체 생성
 soup = BeautifulSoup(page, "html.parser")
+
 # 전체 보기
-print(soup.prettify())
+#print(soup.prettify())
+
+#<p>태그 전체를 검색
+#print(soup.find_all("p"))
+
+#첫번째 <p>만 검색
+#print(soup.find("p"))
+
+#<p class = "outer-text">형태만 검색
+print(soup.find_all("p", class_="outer-text"))
